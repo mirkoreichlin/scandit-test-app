53b5241f9b489647f41b3d6f6367fd18bb2aa5b0
Mirko Reichlin <mirko.reichlin@gmail.com>
2020-07-24 13:34:39 +0200

    lazy load sub module to recreate error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ScanSettings, Barcode, CameraSettings, ScanResult } from 'scandit-sdk-angular';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +6,5 @@ import { ScanSettings, Barcode, CameraSettings, ScanResult } from 'scandit-sdk-a
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public scanSettings = new ScanSettings({
-    enabledSymbologies: [Barcode.Symbology.CODE128],
-    maxNumberOfCodesPerFrame: 1,
-    searchArea: {x: 0.2, y: 0.4, width: 0.6, height: 0.2}
-  });
-  public cameraSettings: CameraSettings = {resolutionPreference: CameraSettings.ResolutionPreference.ULTRA_HD};
 
-  public onScan(scanResult: ScanResult) {
-    alert(scanResult.barcodes[0].data);
-  }
 }
